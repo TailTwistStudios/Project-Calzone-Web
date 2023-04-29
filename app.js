7c91abb8edbebbd4fc7497a93d18c77141e4398e
Evan Overly <eho1@students.uwf.edu>
2023-04-28 21:16:12 -0500

    Initial working project

---
+++ app.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let app = express();
+
+
+
+let handlebars = require("express-handlebars").create({
+    defaultLayout: "main",
+    //helpers: {}
+});
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.set("port", process.env.PORT || 3000);
+
+
+//homepage
+app.get("/", function(req,res) {
+    res.render("home");
+});
+
+//404
+app.use(function (req, res) {
+    res.status(404);
+    res.render("404");
+});
+
+//500
+app.use(function(err,req,res,next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render("500");
+});
+
+app.listen(app.get("port"), function() {
+    console.log("Started running on port " + app.get("port"));
+})
+
