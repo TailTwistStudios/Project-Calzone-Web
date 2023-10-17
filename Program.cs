3575504c9226cfe396ce273724273f37c6f27032
Aetous <Aetous@proton.me>
2023-10-17 17:19:06 -0500

    Intellisense now works

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+
+using Project_Calzone_Web.lib;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
---
+++ lib/RegexLibrary.cs
@@ -0,0 +1,7 @@
+﻿namespace Project_Calzone_Web.lib
+{
+    public class RegexLibrary
+    {
+        public const string IPv4 = "^([0-2]?[0-9]?[0-9])\.([0-2]?[0-9]?[0-9])\.([0-2]?[0-9]?[0-9])\.([0-2]?[0-9]?[0-9])$";
+    }
+}
